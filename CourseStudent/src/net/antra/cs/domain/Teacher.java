4a777758eb3256fc93321af8bf8072dd1cb2b04b
daweizhuang <dawei.zhuang@antra.net>
2017-02-09 15:18:34 -0500

    Added cascade type and fetch type.

--- CourseStudent/src/net/antra/cs/domain/Teacher.java
+++ CourseStudent/src/net/antra/cs/domain/Teacher.java
@@ -1,60 +1,60 @@
-package net.antra.cs.domain;
-
-import java.io.Serializable;
-import java.util.Set;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-
-@Entity
-@Table(name="teacher")
-public class Teacher implements Serializable{
-	private static final long serialVersionUID = 1811519075912415057L;
-	private Integer seqId;
-	private String name;
-	private Integer age;
-	private String inactiveInd = "N";
-	private Set<Course> courses;
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name = "seq_id")
-	public Integer getSeqId() {
-		return seqId;
-	}
-	public void setSeqId(Integer seqId) {
-		this.seqId = seqId;
-	}
-	@Column(name = "name",length=150, nullable=false)
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	@Column(name = "inactive_ind",length=1, nullable=false)
-	public String getInactiveInd() {
-		return inactiveInd;
-	}
-	public void setInactiveInd(String inactiveInd) {
-		this.inactiveInd = inactiveInd;
-	}
-	@Column(name="age",columnDefinition="int")
-	public Integer getAge() {
-		return age;
-	}
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-	@OneToMany(mappedBy="teacher")
-	public Set<Course> getCourses() {
-		return courses;
-	}
-	public void setCourses(Set<Course> courses) {
-		this.courses = courses;
-	}
-}
+//package net.antra.cs.domain;
+//
+//import java.io.Serializable;
+//import java.util.Set;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.OneToMany;
+//import javax.persistence.Table;
+//
+//@Entity
+//@Table(name="teacher")
+//public class Teacher implements Serializable{
+//	private static final long serialVersionUID = 1811519075912415057L;
+//	private Integer seqId;
+//	private String name;
+//	private Integer age;
+//	private String inactiveInd = "N";
+//	private Set<Course> courses;
+//	@Id
+//	@GeneratedValue(strategy=GenerationType.IDENTITY)
+//	@Column(name = "seq_id")
+//	public Integer getSeqId() {
+//		return seqId;
+//	}
+//	public void setSeqId(Integer seqId) {
+//		this.seqId = seqId;
+//	}
+//	@Column(name = "name",length=150, nullable=false)
+//	public String getName() {
+//		return name;
+//	}
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//	@Column(name = "inactive_ind",length=1, nullable=false)
+//	public String getInactiveInd() {
+//		return inactiveInd;
+//	}
+//	public void setInactiveInd(String inactiveInd) {
+//		this.inactiveInd = inactiveInd;
+//	}
+//	@Column(name="age",columnDefinition="int")
+//	public Integer getAge() {
+//		return age;
+//	}
+//	public void setAge(Integer age) {
+//		this.age = age;
+//	}
+//	@OneToMany(mappedBy="teacher")
+//	public Set<Course> getCourses() {
+//		return courses;
+//	}
+//	public void setCourses(Set<Course> courses) {
+//		this.courses = courses;
+//	}
+//}
--- CourseStudent/src/net/antra/test/TestApp2.java
+++ CourseStudent/src/net/antra/test/TestApp2.java
@@ -1,22 +1,115 @@
 package net.antra.test;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.jms.Session;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import net.antra.cs.domain.Course;
+import org.hibernate.Hibernate;
+
+import net.antra.cs.domain2.Course;
+import net.antra.cs.domain2.Student;
+import net.antra.cs.domain2.Teacher;
 
 public class TestApp2 {
 	public static void main(String[] args) {
+//		addCourseAndTeacher();
+//		addCourseAndStudents();
+//		getCourseAndLazyTeacher();
+		getCourseAndLazyTeacher2();
+//		demo();
+	}
+	
+	private static void demo() {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("course_student_teacher_pu");
 		EntityManager em = emf.createEntityManager();
-		Course course = em.find(Course.class, 11);
 		em.getTransaction().begin();
-		em.remove(course);
-		//em.persist(course);
-		em.flush();
+		Course course = em.find(Course.class, 19);
+		Course course1 = em.find(Course.class, 19);
+		Course course2 = em.find(Course.class, 19);
 		em.getTransaction().commit();
 		em.close();
+	}
+
+	public static void addCourseAndTeacher(){
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("course_student_teacher_pu");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		
+		
+		
+		//create a new course
+		Course course = new Course();
+		course.setCredit(1);
+		course.setName("Science");
+		//set teacher to a coures
+		Teacher teacher = new Teacher();
+		teacher.setSeqId(18);
+		teacher.setAge(20);
+		course.setTeacher(teacher);
+		
+		em.merge(course);
+		em.getTransaction().commit();
+		em.close();
+		emf.close();
+	}
+	
+	public static void addCourseAndStudents(){
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("course_student_teacher_pu");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		
+		
+		//create a new course
+		Course course = new Course();
+		course.setCredit(1);
+		course.setName("Art");
+		//set teacher to a coures
+		Set<Student> students = new HashSet<>();
+		Student student1 = new Student();
+		Student student2 = new Student();
+		student1.setName("Student A");
+		student2.setName("Student B");
+		students.add(student1);
+		students.add(student2);
+		course.setStudents(students);
+		
+		
+		em.persist(course);
+		em.getTransaction().commit();
+		em.close();
+		emf.close();
+	}
+	
+	public static void getCourseAndLazyTeacher(){
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("course_student_teacher_pu");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		Course c = em.find(Course.class, 25);
+
+		em.getTransaction().commit();
+		em.close();
+		System.out.println(c.getTeacher().getAge());
+
+		emf.close();
+	}
+	
+	public static void getCourseAndLazyTeacher2(){
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("course_student_teacher_pu");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		Course c = em.find(Course.class, 25);
+		//Hibernate.initialize(c.getTeacher());
+//		c.getTeacher().getName();
+//		Course c = em.createQuery("select c from Course c Join Fetch c.teacher where c.id = 25",Course.class)
+//				   .getSingleResult();
+		em.getTransaction().commit();
+		em.close();
+		System.out.println(c.getTeacher().getAge());
 		emf.close();
 	}
 }
